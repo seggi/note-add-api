40b1af1b60b6ede41609a531db025c56df86a962
nankim <nankim@nankims-MacBook-Pro.local>
2022-05-23 03:35:38 +0200

    setup project route

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main()  {
-	
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.New()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Note Add",
+		})
+	})
+	r.Run()
 }
